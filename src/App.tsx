f219ce55c35903e86ac57fa537ce7eaf9d9a6e6c
Sam <hellousama@protonmail.com>
2021-10-23 18:43:01 +0500

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { db } from "./utils/firebaseConfig";
 const App = () => {
   return(
     <div>
-      <h1>Hello</h1>
+      <h1>Hello World</h1>
     </div>
   )
 };
--- src/utils/firebaseConfig.tsx
+++ src/utils/firebaseConfig.tsx
@@ -3,7 +3,7 @@ import { getFirestore }  from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  // ...
+  // ....
 };
 
 // Initialize Firebase
